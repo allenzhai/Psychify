008d90525ed1c519986d24f2723014ad578096d3
zzheng07 <zzheng07@calpoly.edu>
2020-04-10 01:44:19 -0700

    added jest test framework

--- README.md
+++ README.md
@@ -7,3 +7,13 @@ UI Prototype Line: https://www.figma.com/file/744FuVSeNm89ZIQ5JLpMo7/Psychology-
 
 System Architecture (Feb 7 Class Assignment):
 ![Deployment Diagram](DeploymentDiagram_020920.jpg)
+
+
+### Test
+To run test cases. (Dont forget to install new dependencies first.)
+```
+npm test
+```
+
+#### References
++ [How to test Express.js with Jest and Supertest](https://www.albertgao.xyz/2017/05/24/how-to-test-expressjs-with-jest-and-supertest/)
---
+++ src/server/app.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const path = require('path');
+const api = require('./api');
+const app = express();
+
+app.use(express.static('dist'));
+// Handles any requests that don't match the ones above
+
+app.get('/api/disorders', (req, res) => {
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+  api.listDisorders().then(rows => {
+    console.log(rows);
+    res.json(rows);
+  }).catch(err => {
+    console.log(err);
+    // should return user friendly error message here.
+    // future change is needed here;
+    res.json(err);
+  });
+});
+
+app.get('/api/searchDisorderName/:disorder', (req, res) => {
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+  var disorder = req.params.disorder;
+
+  api.queryName(disorder).then(rows => {
+    console.log(rows);
+    res.json(rows);
+  }).catch(err => {
+    console.log(err);
+    // should return user friendly error message here.
+    // future change is needed here;
+    res.json(err);
+  });
+});
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(process.cwd(), 'dist/index.html'));
+});
+
+module.exports = app;
