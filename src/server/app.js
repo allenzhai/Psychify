4369178701ca5b49027bd9087b0c7c73c271c67f
zzheng07 <zzheng07@calpoly.edu>
2020-04-10 01:48:19 -0700

    fixed bug that client routing doesn't work

--- src/server/app.js
+++ src/server/app.js
@@ -34,7 +34,7 @@ app.get('/api/searchDisorderName/:disorder', (req, res) => {
   });
 });
 
-app.get('/', (req, res) => {
+app.get('/*', (req, res) => {
   res.sendFile(path.join(process.cwd(), 'dist/index.html'));
 });
 
