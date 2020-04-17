2802c654284c24ac79791e198fb8dda0d213932f
allenzhai <azhai@calpoly.edu>
2020-04-16 22:15:29 -0500

    added sort by menu and styling

--- src/client/pages/SearchResults.js
+++ src/client/pages/SearchResults.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { Navbar } from './components/Navbar.js';
 
 import { ResultsSearchbar } from './components/ResultsSearchbar.js';
+import { SortMenu } from './components/SortMenu.js';
 
 import { DisorderResult } from './components/DisorderResult';
 import './components/style/SearchResults.css';
@@ -13,10 +14,30 @@ export class SearchResults extends React.Component {
     super(props);
     this.handleSearchbarUpdate = this.handleSearchbarUpdate.bind(this);
     this.handleSearchbarSubmit = this.handleSearchbarSubmit.bind(this);
+    this.handleSortUpdate = this.handleSortUpdate.bind(this);
     this.state = {
       searchterms: '',
       resultsList: [],
       isLoaded: false,
+      sortByList: [
+        {
+           key : 0,
+           title : 'Name'
+        },
+        {
+            key : 1,
+            title : 'Alias'
+        },
+        {
+            key : 2,
+            title : 'Sub-Category'
+        },
+        {
+            key : 3,
+            title : 'Category'
+        }
+    ],
+    sortBy: 'Name'
     };
   }
 
@@ -24,8 +45,13 @@ export class SearchResults extends React.Component {
     this.setState({ searchterms });
   }
 
+  handleSortUpdate(sortBy){
+    this.setState({ sortBy });
+  }
+
   handleSearchbarSubmit(searchterms) {
     // alert(this.state.searchterms);
+    
     if (!(this.state.searchterms)) {
       fetch('http://localhost:5000/api/disorders')
         .then(res => res.json())
@@ -45,7 +71,7 @@ export class SearchResults extends React.Component {
         .then(res => res.json())
         .then(
           (serverResult) => {
-          // console.log(JSON.stringify(serverResult));
+          console.log(JSON.stringify(serverResult));
             const filteredServerResult = this.filterServerResult(serverResult);
             JSON.stringify(filteredServerResult);
             this.setState({
@@ -57,24 +83,70 @@ export class SearchResults extends React.Component {
     }
   }
 
+
+
   filterServerResult(serverResult) {
     const matchArray = [];
     let maxMatch = -1;
     let idx;
     let result;
     const ans = [];
+    let A;
+    let B;
+
     serverResult.sort((a, b) => {
-      const A = a.name.toUpperCase();
-      const B = b.name.toUpperCase();
-      return (A < B) ? -1 : (A > B) ? 1 : 0;
+      A = a.category.toUpperCase();
+      B = b.category.toUpperCase();
+      if (this.state.sortBy == "Name"){
+          A = a.name.toUpperCase();
+          B = b.name.toUpperCase();
+      }
+      else if (this.state.sortBy == "Alias"){
+          A = a.alias.toUpperCase();
+          B = b.alias.toUpperCase();
+      }
+      else if (this.state.sortBy == "Sub-Category") {
+          A = a.sub_category.toUpperCase();
+          B = b.sub_category.toUpperCase();
+      }
+      else if (this.state.sortBy == "Category"){
+          A = a.category.toUpperCase();
+          B = b.category.toUpperCase();
+      }
+
+      if (A == ''){
+        return 1;
+      }
+      else if (B == ''){
+        return -1;
+      }
+      else{
+        return (A < B) ? -1 : (A > B) ? 1 : 0;
+      }
     });
-    // console.log(this.state.searchterms);
+
     if (this.state.searchterms != null) {
       for (result = 0; result < serverResult.length; result++) {
-        const x = this.MatchCount(serverResult[result].name.split(''));
-        matchArray.push(x);
+
+        if (this.state.sortBy == "Name"){
+            const x = this.MatchCount(serverResult[result].name.split(''));
+            matchArray.push(x);
+        }
+        else if (this.state.sortBy == "Alias"){
+            const x = this.MatchCount(serverResult[result].alias.split(''));
+            matchArray.push(x);
+        }
+        else if (this.state.sortBy == "Sub-Category") {
+            const x = this.MatchCount(serverResult[result].sub_category.split(''));
+            matchArray.push(x);
+        }
+        else if (this.state.sortBy == "Category"){
+            const x = this.MatchCount(serverResult[result].category.split(''));
+            matchArray.push(x);
+        }
+        
       }
-      // console.log(matchArray);
+
       while (ans.length < serverResult.length) {
         maxMatch = -1;
         for (result = 0; result < matchArray.length; result++) {
@@ -83,12 +155,10 @@ export class SearchResults extends React.Component {
             idx = result;
           }
         }
-        // console.log(serverResult[idx])
         ans.push(serverResult[idx]);
         matchArray[idx] = -1;
       }
     }
-    // console.log(matchArray);
     return ans;
   }
 
@@ -107,18 +177,28 @@ export class SearchResults extends React.Component {
   }
 
   render() {
-    const { searchterms } = this.state;
+    const { searchterms, sortBy } = this.state;
+
     return (
       <div className="results">
         <Navbar />
         <div className="search-results-container">
           <h2 className="search-results-title">Search Results</h2>
+          <SortMenu
+            list={this.state.sortByList}
+            searchterms={searchterms}
+            onSortSubmit={this.handleSearchbarSubmit}
+            onSortUpdate = {this.handleSortUpdate}
+            sortBy = {sortBy}
+          />
           <ResultsSearchbar
             className="search-bar"
             searchterms={searchterms}
             onSearchbarUpdate={this.handleSearchbarUpdate}
             onSearchbarSubmit={this.handleSearchbarSubmit}
           />
+          <p className = "sortedBy"> Sorted by {sortBy}</p>
+
           {/* <h2 className = "example"> example: {JSON.stringify(this.state.example)} </h2> */}
           <div className="results-entries">
             {this.state.isLoaded
