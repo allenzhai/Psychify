83bed45cd3877b4fc786370d882ece24d335669b
allenzhai <azhai@calpoly.edu>
2020-04-24 22:19:53 -0500

    alphabetized and stylized index page with a media breakpoint

--- src/client/pages/DSM.js
+++ src/client/pages/DSM.js
@@ -2,6 +2,8 @@ import React from 'react';
 import Navbar from '../components/Navbar';
 import Searchbar from '../components/Searchbar';
 
+import "../style/DSM.css";
+
 export default function DSM() {
   // Blank placeholder component for the DSM Index
   return (
@@ -9,32 +11,39 @@ export default function DSM() {
       <Navbar />
       <div className="landing-content">
         <Searchbar className="searchbar-home" />
-        <br />
-        <h1>Diagnostic Criteria and Codes</h1>
-        <ul className="disorder-category" style={{ listStyleType: 'none' }}>
-          <li><a href="/">Neurodevelopmental Disorders</a></li>
-          <li><a href="/stuff">Schizophrenia Spectrum and Other Psychotic Disorders</a></li>
-          <li><a href="/contact">Bipolar and Related Disorders</a></li>
-          <li><a href="/">Depressive Disorders</a></li>
-          <li><a href="/stuff">Anxiety Disorders</a></li>
-          <li><a href="/contact">Obsessive-Compulsive and Related Disorders</a></li>
-          <li><a href="/">Trauma- and Stressor-Related Disorders</a></li>
-          <li><a href="/stuff">Dissociative Disorders</a></li>
-          <li><a href="/contact">Somatic Symptom and Related Disorders</a></li>
-          <li><a href="/">Feeding and Eating Disorders</a></li>
-          <li><a href="/stuff">Elimination Disorders</a></li>
-          <li><a href="/contact">Sleep-Wake Disorders</a></li>
-          <li><a href="/">Sexual Dysfunctions</a></li>
-          <li><a href="/stuff">Gender Dysphoria</a></li>
-          <li><a href="/contact">Disruptive, Impulse-Control, and Conduct Disorders</a></li>
-          <li><a href="/">Substance-Related and Addictive Disorders</a></li>
-          <li><a href="/stuff">Neurocognitive Disorders</a></li>
-          <li><a href="/contact">Personality Disorders</a></li>
-          <li><a href="/">Paraphilic Disorders</a></li>
-          <li><a href="/stuff">Other Mental Disorders and Additional Codes</a></li>
-          <li><a href="/contact">Medication-Induced Movement Disorders and Other Adverse Effects of Medication</a></li>
-          <li><a href="/">Other Conditions That May Be a Focus of Clinical Attention</a></li>
-        </ul>
+      </div>
+      <div className="index-list-con">
+        <h1 className="index-header">Diagnostic Criteria and Codes Index</h1>
+        <div className="col">
+          <ul className="index-list" style={{ listStyleType: 'none' }}>
+            <li><a href="/">Anxiety Disorders</a></li>
+            <li><a href="/">Bipolar and Related Disorders</a></li>
+            <li><a href="/">Depressive Disorders</a></li>
+            <li><a href="/">Disruptive, Impulse-Control, and Conduct Disorders</a></li>
+            <li><a href="/">Dissociative Disorders</a></li>
+            <li><a href="/">Elimination Disorders</a></li>
+            <li><a href="/">Feeding and Eating Disorders</a></li>
+            <li><a href="/">Gender Dysphoria</a></li>
+            <li><a href="/">Medication-Induced Movement Disorders and Other Adverse Effects of Medication</a></li>
+            <li><a href="/">Neurocognitive Disorders</a></li>
+            <li><a href="/">Neurodevelopmental Disorders</a></li>
+          </ul>
+        </div>
+        <div className="col">
+          <ul className="index-list" style={{ listStyleType: 'none' }}>
+            <li><a href="/">Obsessive-Compulsive and Related Disorders</a></li>
+            <li><a href="/">Other Conditions That May Be a Focus of Clinical Attention</a></li>
+            <li><a href="/">Other Mental Disorders and Additional Codes</a></li>
+            <li><a href="/">Paraphilic Disorders</a></li>
+            <li><a href="/">Personality Disorders</a></li>
+            <li><a href="/">Schizophrenia Spectrum and Other Psychotic Disorders</a></li>
+            <li><a href="/">Sexual Dysfunctions</a></li>
+            <li><a href="/">Sleep-Wake Disorders</a></li>
+            <li><a href="/">Somatic Symptom and Related Disorders</a></li>
+            <li><a href="/">Substance-Related and Addictive Disorders</a></li>
+            <li><a href="/">Trauma- and Stressor-Related Disorders</a></li>
+          </ul>
+        </div>
       </div>
     </body>
   );
