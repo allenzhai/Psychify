d3b46044fa889fdb96d9f639ba2541a270c07d9f
Andrii-I <ieroshenkoa@gmail.com>
2020-05-01 11:02:47 -0700

    WIP displaying API data

--- src/client/pages/Profile.js
+++ src/client/pages/Profile.js
@@ -1,8 +1,6 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useHistory, useLocation } from 'react-router-dom';
 import Navbar from '../components/Navbar';
-import Searchbar from '../components/Searchbar';
-
 
 import Button from '../components/Button';
 
@@ -14,6 +12,9 @@ function Profile() {
   const [username, setUsername] = useState('Username');
   const [email, setEmail] = useState('username@gmail.com');
   const [about, setAbout] = useState('Ernest Miller Hemingway (July 21, 1899 – July 2, 1961) was an American journalist, novelist, short-story writer, and sportsman. His economical and understated style—which he termed the iceberg theory—had a strong influence on 20th-century fiction, while his adventurous lifestyle and his public image brought him admiration from later generations. Hemingway produced most of his work between the mid-1920s and the mid-1950s, and he won the Nobel Prize in Literature in 1954. He published seven novels, six short-story collections, and two nonfiction works. Three of his novels, four short-story collections, and three nonfiction works were published posthumously. Many of his works are considered classics of American literature. ');
+  const [ID, setID] = useState(1);
+
+
 
   const handleEdit = () => {
 
