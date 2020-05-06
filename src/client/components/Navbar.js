b32e743b54ef1aec5672db5c4f9b9fb540c4036b
allenzhai <azhai@calpoly.edu>
2020-05-06 01:06:56 -0500

    added bars menu for smaller screen sizes in navbar

--- src/client/components/Navbar.js
+++ src/client/components/Navbar.js
@@ -1,11 +1,11 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 
 import '../style/Navbar.css';
 
 
 function Navbar(props) {
   const { loggedIn, username } = props;
-  console.log(props);
+  // console.log(props);
   const component = loggedIn
     ? (
       <nav className="profile">
@@ -18,11 +18,23 @@ function Navbar(props) {
         <a href="#registration" className="nav-item signup">Sign Up</a>
       </>
     );
+  const nav = useRef(null);
+
+  function toggleNav() {
+    if (nav.current.className === 'navbar') {
+      nav.current.className += '-responsive';
+    } else {
+      nav.current.className = 'navbar';
+    }
+  }
 
   return (
     <div className="top-bar">
       <a className="homepage" href="/">Psychify</a>
-      <nav className="navbar">
+      <nav className="navbar" ref={nav}>
+        <button type="button" className="icon" onClick={() => toggleNav()}>
+          <i className="fas fa-bars" />
+        </button>
         <a className="nav-item" href="/forum">Forum</a>
         <a className="nav-item" href="/dsm">Index</a>
         {component}
