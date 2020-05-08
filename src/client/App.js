0e2a9e475da21444d4cb74e78aaa0f7f7fa604aa
allenzhai <azhai@calpoly.edu>
2020-05-07 21:22:22 -0500

    added user context provider in app.js

--- src/client/App.js
+++ src/client/App.js
@@ -12,7 +12,7 @@ import Navbar from './components/Navbar';
 
 export default function App() {
   const [loggedIn, setLoggedIn] = React.useState(false);
-  const [username, changeUsername] = React.useState('');
+  const [username, changeUsername, token] = React.useState('');
 
   function loginUser() {
     setLoggedIn(true);
@@ -21,19 +21,26 @@ export default function App() {
   function setUsername(user) {
     changeUsername(user);
   }
+  // user context
+  const UserContext = React.createContext({
+    username: { username },
+    token: { token }
+  });
 
   return (
     <>
       <Navbar loggedIn={loggedIn} username={username} />
-      <Switch>
-        <Route exact path="/" component={Home} />
-        <Route path="/forum" component={Forum} />
-        <Route path="/dsm" component={DSM} />
-        <Route path="/profile" component={Profile} />
-        <Route path="/results" component={SearchResults} />
-      </Switch>
-      <Route path="/" render={props => <LoginModal {...props} loginUser={loginUser} setUsername={setUsername} />} />
-      <Route path="/" render={props => <RegistrationModal {...props} loginUser={loginUser} setUsername={setUsername} />} />
+      <UserContext.Provider value={token}>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/forum" component={Forum} />
+          <Route path="/dsm" component={DSM} />
+          <Route path="/profile" component={Profile} />
+          <Route path="/results" component={SearchResults} />
+        </Switch>
+        <Route path="/" render={props => <LoginModal {...props} loginUser={loginUser} setUsername={setUsername} />} />
+        <Route path="/" render={props => <RegistrationModal {...props} loginUser={loginUser} setUsername={setUsername} />} />
+      </UserContext.Provider>
     </>
   );
 }
