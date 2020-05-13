f6cd5ad52e482447d80cdd6aab43b1ed60e436e0
ethanbwaite <ebwaite@calpoly.edu>
2020-05-12 17:10:36 -0700

    Small fix in CommentList again

--- src/client/components/CommentList.js
+++ src/client/components/CommentList.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-import ForumComment from './ForumComment.js';
+import ForumComment from './ForumComment';
 
 function CommentList({ datasource }) {
   console.log(datasource);
